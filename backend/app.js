810db36d4cdf0fbd6dfe76340b7b874cd141db65
Vladimir <vvk.code@ya.ru>
2023-09-05 13:21:01 +0300

    run in prod

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,21 @@ const cors = require('cors')
 const app = express()
 const port = 3500
 
-app.use(cors());
+// app.use(cors());
+app.use(express.static('public'));
 
 const habr = require('./habr-rss');
 
-app.get('/', (req, res) => {
-  res.send('-');
-})
+// app.get('/', (req, res) => {
+//   res.send('-');
+// })
 
 app.get('/habr/', habr.list);
 app.get('/habr/:id', habr.get);
+
+app.get('*', (req, res) => {
+  res.sendFile('/app/public/index.html');
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
